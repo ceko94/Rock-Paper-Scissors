13c77f09fba8084890f0b11408ae98b06f6fc92e
ceko94 <163397735+ceko94@users.noreply.github.com>
2025-05-07 08:13:36 +0200

    Update script.js
    declared a variable with Math random *3 to create 3 possible options, rounded those options up with the math.floor method and used an if statement to explain which return value should appear

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-
+function getComputerChoice(){
+  let randomNumber= Math.random()*3;
+  let randomInt = Math.floor(randomNumber);
+  if(randomInt === 0){
+  return "rock";
+  } else if (randomInt === 1){
+  return "paper";
+  } else {
+  return "scissors";
+  }
+}
